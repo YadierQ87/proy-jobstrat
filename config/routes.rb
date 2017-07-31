ff69f452550356674b2bc1d54550e9f503955213
Yadier Abel de Quesada <yadier.quesada@etecsa.cu>
2017-07-31 00:00:53 -0400

    frontend

--- config/routes.rb
+++ config/routes.rb
@@ -1,54 +1,26 @@
 Rails.application.routes.draw do
 
-  resources :countries
+  get 'home', to: 'home#index'
+       root :to => "home#index"
   get 'information/index'
-
   get 'information/jobs'
-
   get 'information/employees'
-
   get 'information/companies'
-
   get 'information/categories'
-
   get 'information/messages'
-
   get 'information/usersettings'
-
   get 'information/aplicationsms'
-
   resources :messageemployees
-
+  resources :countries
   resources :aplicationjobs
-
   resources :categories
-
   resources :companies
-
   resources :employees
-
   resources :jobs
-
   resources :articles
-
   resources :users
 
-  get 'welcome/index'
-      root :to => "welcome#index"
-
-  get 'home', to: 'home#index'
-
-  get 'administration', to: 'administration#index'
-
-  get 'administration/index'
-
-  get 'administration/jobs'
-
-  get 'administration/employees'
-
-  get 'administration/companies'
-
-  get 'administration/categories'
+  resources :administration
 
 
 end
