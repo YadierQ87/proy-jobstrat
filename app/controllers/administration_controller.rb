e8fa77fab7dfedf3a077bd4bfe1e99c612e08720
Yadier Abel de Quesada <yadier.quesada@etecsa.cu>
2017-08-29 19:03:13 -0400

    arreglos v1.0.2

--- app/controllers/administration_controller.rb
+++ app/controllers/administration_controller.rb
@@ -2,6 +2,20 @@ class AdministrationController < ApplicationController
   #layout :admin_layout
   def index
     @title = 'Administration'
+    if request.get?
+      session[:user_id] = nil
+      @user = User.new
+    else
+      @user = User.new(params[:user])
+      logged_in_user = @user.try_to_login
+
+      if logged_in_user
+        session[:user_id] = logged_in_user.id
+        redirect_to(:action => "index")
+      else
+        flash[:notice] = "Invalid user/password combination"
+      end
+    end
   end
 
   def jobs
